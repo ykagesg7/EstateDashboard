91a1b9919d7609e50a573ee51697177a51df43f6
ykagesg7 <52264421+ykagesg7@users.noreply.github.com>
2024-12-28 18:10:58 +0900

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Ensure environment variables are defined
-const supabaseUrl = 'https://amrleuqngqtfbrjadcky.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcmxldXFuZ3F0ZmJyamFkY2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2MzI1NzAsImV4cCI6MjAyNTIwODU3MH0.Ij0rqgqT6oUmYQGQj_hGIEGYy_8Uj_qEWGtHXt4YC7g'
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables')
-}
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
