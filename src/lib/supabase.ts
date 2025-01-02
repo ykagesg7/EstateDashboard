3de94dab1b945cccbf5ebc6ad9476cf103f3894b
ykagesg7 <52264421+ykagesg7@users.noreply.github.com>
2025-01-02 13:07:13 +0900

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://amrleuqngqtfbrjadcky.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcmxldXFuZ3F0ZmJyamFkY2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2NTI0MDAsImV4cCI6MjAyNTIyODQwMH0.qA8kHlhJMYwWRGgX7GQ5D4lLn9wKI_dKFRHJQ-lUaXM';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcmxldXFuZ3F0ZmJyamFkY2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUxNjAxODMsImV4cCI6MjA0MDczNjE4M30.W6eSJuFudqJ5sZJ0BeauHzODvpqhFNLMWDkgJIevV-8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
